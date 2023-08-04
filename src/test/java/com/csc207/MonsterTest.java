b946b3df03cdda9094fffba5f659bf67dd99cbf2
HW.T430 <HW@T430>
2023-08-04 16:51:17 -0400

    write a test on Grow method

--- src/test/java/com/csc207/MonsterTest.java
+++ src/test/java/com/csc207/MonsterTest.java
@@ -1,4 +1,30 @@
 package com.csc207;
+import org.junit.*;
+
+import static org.junit.Assert.*;
 
 public class MonsterTest {
+    private Monster alien;
+
+    @Before
+    public void setUp(){
+        System.out.println("Setting up...");
+        alien = new Monster("Honk", 100, 60);
+    }
+
+    @After
+    public void tearDown(){
+        System.out.println("Tearing down...");
+        alien = null;
+    }
+
+    @Test
+    public void testGrow(){
+        int beforeSize = alien.size();
+        alien.grow();
+        int afterSize = alien.size();
+        assertEquals(afterSize,beforeSize * 2);
+    }
+
+
 }
